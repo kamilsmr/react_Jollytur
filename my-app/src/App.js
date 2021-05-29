5cece08dc03feb670cfc5251d6e436f350ff281e
kamilsmr <kamils6035@gmail.com>
2021-05-29 16:59:04 +0300

    start

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      {/* todo  apiden veri çekilecek, loading, tour listin içerisine atılacak. Metariul ui kullanıcağız*/}
+
+
+
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
